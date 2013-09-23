ad9b9c1f7369807acfc514158af5e26ebbaf73d8
Mike Funk <mike@mikefunk.com>
2013-09-23 14:31:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,25 +29,25 @@ cd /usr/local/Library/Formula
 curl https://gist.github.com/cweagans/6141478/raw/aea352bf2914832515a5a1f3529e830c7b97c468/- | git apply
 brew install ctags --HEAD
 
-# Symlink the configuration files into their appropriate homes
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/config ~/.config
-ln -s ~/.dotfiles/gitignore ~/.gitignore
-ln -s ~/.dotfiles/profile ~/.profile
-ln -s ~/.dotfiles/screenrc ~/.screenrc
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/grcat ~/.grcat
-ln -s ~/.dotfiles/my.cnf ~/.my.cnf
-ln -s ~/.dotfiles/my.ini ~/.my.ini
-ln -s ~/.dotfiles/inputrc ~/.inputrc
-ln -s ~/.dotfiles/rainbarf.conf ~/.rainbarf.conf
-ln -s ~/.dotfiles/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/.dotfiles/vimrc.local ~/.vimrc.local
-ln -s ~/.dotfiles/vimrc.before ~/.vimrc.before
-ln -s ~/.dotfiles/tmuxinator ~/.tmuxinator
-ln -s ~/.dotfiles/ctags ~/.ctags
-ln -s ~/.dotfiles/999-my-php.ini /usr/local/php5/php.d/999-my-php.ini
-ln -s ~/.dotfiles/999-my-httpd.conf /etc/apache2/other/999-my-httpd.conf
+# Symlink the configuration files into their appropriate homes if they don't already exist
+[ -f ~/.gitconfig ] && ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+[ -f ~/.config ] && ln -s ~/.dotfiles/config ~/.config
+[ -f ~/.gitignore ] && ln -s ~/.dotfiles/gitignore ~/.gitignore
+[ -f ~/.profile ] && ln -s ~/.dotfiles/profile ~/.profile
+[ -f ~/.screenrc ] && ln -s ~/.dotfiles/screenrc ~/.screenrc
+[ -f ~/.tmux.conf ] && ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+[ -f ~/.grcat ] && ln -s ~/.dotfiles/grcat ~/.grcat
+[ -f ~/.my.cnf ] && ln -s ~/.dotfiles/my.cnf ~/.my.cnf
+[ -f ~/.my.ini ] && ln -s ~/.dotfiles/my.ini ~/.my.ini
+[ -f ~/.inputrc ] && ln -s ~/.dotfiles/inputrc ~/.inputrc
+[ -f ~/.rainbarf.conf ] && ln -s ~/.dotfiles/rainbarf.conf ~/.rainbarf.conf
+[ -f ~/.vimrc.bundles.local ] && ln -s ~/.dotfiles/vimrc.bundles.local ~/.vimrc.bundles.local
+[ -f ~/.vimrc.local ] && ln -s ~/.dotfiles/vimrc.local ~/.vimrc.local
+[ -f ~/.vimrc.before.local ] && ln -s ~/.dotfiles/vimrc.before.local ~/.vimrc.before.local
+[ -f ~/.tmuxinator ] && ln -s ~/.dotfiles/tmuxinator ~/.tmuxinator
+[ -f ~/.ctags ] && ln -s ~/.dotfiles/ctags ~/.ctags
+[ -f /usr/local/php5/php.d/999-my-php.ini ] && ln -s ~/.dotfiles/999-my-php.ini /usr/local/php5/php.d/999-my-php.ini
+[ -f /etc/apache2/other/999-my-httpd.conf ] && ln -s ~/.dotfiles/999-my-httpd.conf /etc/apache2/other/999-my-httpd.conf
 
 # install vim packages
-vim +BundleInstall +qall
+vim +BundleInstall +BundleClean! +qall
