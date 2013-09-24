deed399b7bbb4f08b1dc012e04db49e60f182763
Mike Funk <mike@mikefunk.com>
2013-09-24 14:35:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # pretty colors
-GREEN="\e[42m"
-RESET="\e[49m"
+GREEN='\e[42m'
+RESET='\e[49m'
 
 # install homebrew stuff
-echo -e "$GREEN installing homebrew... $RESET"
+echo -e "${GREEN} installing homebrew... ${RESET}"
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 brew install ack
 brew install bash-completion
@@ -26,23 +26,23 @@ brew install wget
 brew linkapps
 
 # install cli stuff
-echo -e "$GREEN installing cli tools... $RESET"
+echo -e "${GREEN} installing cli tools... ${RESET}"
 sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
 sudo chmod a+x /usr/local/bin/php-cs-fixer
 
 # install gems
-echo -e "$GREEN installing gems... $RESET"
+echo -e "${GREEN} installing gems... ${RESET}"
 gem install pygmentize
 
 # install ctags patched
 # @url https://github.com/shawncplus/phpcomplete.vim/wiki/Patched-ctags
-echo -e "$GREEN installing ctags patched... $RESET"
+echo -e "${GREEN} installing ctags patched... ${RESET}"
 cd /usr/local/Library/Formula
 curl https://gist.github.com/cweagans/6141478/raw/aea352bf2914832515a5a1f3529e830c7b97c468/- | git apply
 brew install ctags --HEAD
 
 # Symlink the configuration files into their appropriate homes if they don't already exist
-echo -e "$GREEN installing symlinks... $RESET"
+echo -e "${GREEN} installing symlinks... ${RESET}"
 [ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 [ ! -f ~/.config ] && ln -s ~/.dotfiles/config ~/.config
 [ ! -f ~/.ssh/config ] && ln -s ~/.dotfiles/ssh/config ~/.ssh/config
@@ -64,18 +64,18 @@ echo -e "$GREEN installing symlinks... $RESET"
 [ ! -f /etc/apache2/other/999-my-httpd.conf ] && sudo ln -s ~/.dotfiles/999-my-httpd.conf /etc/apache2/other/999-my-httpd.conf
 
 # install vim packages
-echo -e "$GREEN installing vim packages... $RESET"
+echo -e "${GREEN} installing vim packages... ${RESET}"
 vim +BundleInstall +BundleClean! +qall
 
 # install youcompleteme
-echo -e "$GREEN installing youcompleteme... $RESET"
+echo -e "${GREEN} installing youcompleteme... ${RESET}"
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
 cd -
 
 # prepend to /etc/paths
-echo -e "$GREEN prepending to paths... $RESET"
+echo -e "${GREEN} prepending to paths... ${RESET}"
 sudo cp /etc/paths /etc/paths_BACKUP
 echo -e "/usr/local/bin"|cat - /etc/paths > /tmp/out && sudo mv /tmp/out /etc/paths
 
-echo -e "$GREEN install complete $RESET"
+echo -e "${GREEN} install complete ${RESET}"
