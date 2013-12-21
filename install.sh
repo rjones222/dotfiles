58872fe3683d20ee6db23121278e8e8ac4aaa84b
Mike Funk <mfunk@internetbrands.com>
2013-12-20 17:10:56 -0800

    updates

--- install.sh
+++ install.sh
@@ -152,6 +152,29 @@ if [[ "$(type -P apt-get)" ]]; then
 
     # install linux packages
     e_header "Installing apt-get Packages"
+
+    sudo apt-get install -y openjdk-7-jre
+    sudo apt-get install -y git
+    sudo apt-get install -y git-core
+    sudo apt-get install -y openssh-client
+    sudo apt-get install -y openssh-server
+    sudo apt-get install -y postfix
+    sudo apt-get install -y cronolog
+    sudo apt-get install -y apache2
+    sudo apt-get install -y imagemagick
+    sudo apt-get install -y postgresql
+    sudo apt-get install -y postgresql-contrib
+    sudo apt-get install -y pgadmin3
+    sudo apt-get install -y php5
+    sudo apt-get install -y libapache2-mod-php5
+    sudo apt-get install -y php5-xdebug
+    sudo apt-get install -y php5-imagick
+    sudo apt-get install -y php-apc
+    sudo apt-get install -y php5-cli
+    sudo apt-get install -y php5-pgsql
+    sudo apt-get install -y php5-curl
+    sudo apt-get install -y vim
+
     sudo apt-get install -y ack
     sudo apt-get install -y ctags
     sudo apt-get install -y git-extras
@@ -167,7 +190,7 @@ if [[ "$(type -P apt-get)" ]]; then
     sudo apt-get install -y tmux
     sudo apt-get install -y tree
     # sudo apt-get install -y vagrant
-    sudo apt-get install -y vim
+    # sudo apt-get install -y vim
     # sudo apt-get install vim-nox
 
     # install rbenv
