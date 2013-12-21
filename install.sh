fa5a66b8ffee89d785235df8657159cf4c6c6a11
Mike Funk <mfunk@internetbrands.com>
2013-12-20 18:39:47 -0800

    updates

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 
 # sudo
-e_header "You'll need sudo soon"
+e_header "Verifying sudo password"
 sudo echo -e " \033[1;32m✔\033[0m Sudo password entered"
 
 # Given a list of desired items and installed items, return a list
