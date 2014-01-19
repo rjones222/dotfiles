73ca0d0f2310cc5ce4d048918288530fc2b51b03
Mike Funk <mfunk@internetbrands.com>
2014-01-19 10:48:18 -0800

    fixed symlink task

--- tasks/04_symlinks.sh
+++ tasks/04_symlinks.sh
@@ -8,30 +8,31 @@ function link_this() {
     # if the file/dir to link doesn't exist
     if [[ ! -f "$1" ]] && [[ ! -d "$1" ]]; then
         log_error "file/directory $1 does not exist"
-    return ${E_FAILURE}
+        return ${E_FAILURE}
+    fi
 
     # if the symlink exists, skip it and notify
-    elif [[ -L "$2" ]]; then
+    if [[ -L "$2" ]]; then
         log_info "symlink $2 exists"
         return
+    fi
 
     # if the file already exists
-    elif [[ -f $2 || -d $2 ]]; then
+    if [[ -f $2 || -d $2 ]]; then
 
         # create a backup dir if it doesn't already exist and notify
-    log_info "creating backup directory"
+        log_info "creating backup directory"
         backup_dir="$HOME/backup/"
         [[ -d "$backup_dir" ]] || sudo mkdir -p "$backup_dir"
 
         # move the file to the backup dir and notify
         log_info "backing up $2"
         sudo mv $2 $backup_dir
+    fi
 
     # else symlink it and notify
-    else
-        log_info "linking $1 to $2"
-        sudo ln -s $1 $2
-    fi
+    log_info "linking $1 to $2"
+    sudo ln -s $1 $2
 }
 
 function symlinks_run() {
