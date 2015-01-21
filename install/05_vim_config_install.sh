b90e849ff6bc4e0dfe26cffa5e14be8663bb8bd8
Mike Funk <mike@mikefunk.com>
2015-01-20 16:43:09 -0800

    updates

--- install/05_vim_config_install.sh
+++ install/05_vim_config_install.sh
@@ -27,8 +27,8 @@ link_this "$HOME/.dotfiles/to_link/vim/.vimrc.plugins" "$HOME/.vimrc.plugins"
 
 # create ~/.vim and ~/.vim/autoload if it doesn't exist
 log_info "creating required vim directories"
-[[ ! -d $HOME/.vim ]] && log_info "creating the ~/.vim directory" && sudo mkdir $HOME/.vim
-[[ ! -d $HOME/.vim ]] && log_info "creating the ~/.vim/autoload directory" && sudo mkdir $HOME/.vim/autoload
+[[ -d $HOME/.vim ]] || log_info "creating the ~/.vim directory" && sudo mkdir $HOME/.vim
+[[ -d $HOME/.vim ]] || log_info "creating the ~/.vim/autoload directory" && sudo mkdir $HOME/.vim/autoload
 
 # install vim-plug
 log_info "installing vim-plug"
