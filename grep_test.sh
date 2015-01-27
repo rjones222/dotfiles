f0eb70ead1dc2d466580d53f7a3e3a80ba5d4680
Mike Funk <mike@mikefunk.com>
2015-01-27 11:32:26 -0800

    updates

--- grep_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# if grep -Fxq 'variables_order = "GPCS"' /usr/local/etc/php/5.6/php.ini
-if grep -Fxq 'variables_order = "EGPCS"' /usr/local/etc/php/5.6/php.ini
-then
-    echo 'yup'
-else
-    echo 'nope'
-fi
