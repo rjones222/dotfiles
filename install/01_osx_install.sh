f8010a169e618a7ea153d8e533e84af4c0e1a343
Mike Funk <mike@mikefunk.com>
2015-01-28 15:51:52 -0800

    updates

--- install/01_osx_install.sh
+++ install/01_osx_install.sh
@@ -127,8 +127,17 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
         # jabber/xmpp client to do chat in the terminal
         if [[ ! "$(type -P profanity)" ]]; then
             log_info "installing profanity console IM client"
-            brew install profanity --with-terminal-notifier 
+            brew install profanity --with-terminal-notifier
         fi
+
+        # homebrew cask
+        # @link https://github.com/caskroom/homebrew-cask
+        brew install caskroom/cask/brew-cask
+
+        # install all quicklook plugins via brew cask
+        # @link https://github.com/sindresorhus/quick-look-plugins
+        log_info "installing all quicklook plugins"
+        brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
         # https://github.com/neovim/neovim/wiki/Installing
         # bookmarking this for later. right now some things are still breaking
@@ -244,7 +253,7 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php56.plist
     # if extension not in httpd.conf, add it
     LoadPhp="LoadModule php5_module /usr/local/opt/php56/libexec/apache2/libphp5.so";
-    if ! grep -Fxq $LoadPhp; then
+    if ! grep -Fxq "/usr/local/opt/php56/libexec/apache2/libphp5.so"; then
         log_info "adding php5 module to apache2 httpd.conf"
         sudo sed -i '.bak' 's/#LoadModule php5_module\ libexec\/apache2\/libphp5\.so/LoadModule php5_module\ \/usr\/local\/opt\/php56\/libexec\/apache2\/libphp5\.so/' /etc/apache2/httpd.conf
     fi
